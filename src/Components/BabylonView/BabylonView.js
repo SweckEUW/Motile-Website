3a3b8b5fd0715956f574d42ee779fe227a33b536
Simon Weck <Simon.weck@raumtaenzer.com>
2021-10-10 17:27:03 +0200

    update babylon scene

--- src/Components/BabylonView/BabylonView.js
+++ src/Components/BabylonView/BabylonView.js
@@ -14,25 +14,31 @@ function ThreeView(){
     // init scene
     let scene = new BABYLON.Scene(engine);
     scene.clearColor = BABYLON.Color3.White();
-    scene.debugLayer.show();
+    // scene.debugLayer.show();
     
     // init camera
-    let camera = new BABYLON.ArcRotateCamera("Camera", 1,.7 ,300,new BABYLON.Vector3(-20,90,20),scene); 
+    let camera = new BABYLON.ArcRotateCamera("Camera", 1,1 ,200,new BABYLON.Vector3(-40,80,0),scene); 
 		camera.attachControl(myRef.current);
+
+    // init Light
+    new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 1, 0), scene);
 
     let assetsManager = new BABYLON.AssetsManager( scene );
     assetsManager.onTaskErrorObservable.add(function(task) {
-        console.log('Task failed', task.errorObject.message, task.errorObject.exception);
+      console.log('Task failed', task.errorObject.message, task.errorObject.exception);
     });
 
+    let phone;
     var phoneTask = assetsManager.addMeshTask("", "", "","./models/Phone-complete.glb");
     phoneTask.onSuccess = (task) => {     
-     let phone = task.loadedMeshes[0];
-     phone.name = "Phone"
+     phone = task.loadedMeshes[0];
+     phone.name = "Phone";
     }
     assetsManager.load();
     
-    engine.runRenderLoop(() => {scene.render()});
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
   
   });
 

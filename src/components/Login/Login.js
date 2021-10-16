36fd11e239be4851b87208eebe9018aa679530ba
Simon Weck <Simon.weck@raumtaenzer.com>
2021-10-16 15:41:23 +0200

    added login/register functions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,32 @@
 import './Login.css';
 import { FaSignInAlt } from 'react-icons/fa';
 import React, {useState} from 'react';
+import AxiosHelper from "./AxiosHelper.js";
 
 function Login(){
   const [loginDialogueVisible, setLoginDialogueVisible] = useState(false);
 
   function toggleLoginDialogue(){
     setLoginDialogueVisible(!loginDialogueVisible);
+  }
+
+  function login(){
+    let name = document.getElementById("login-name").value;
+    let pw = document.getElementById("login-pw").value;
+    
+    // call to server to login
+    // AxiosHelper.
+  }
+
+  function register(){
+    let name = document.getElementById("register-name").value;
+    let pw = document.getElementById("register-pw").value;
+    
+    // call to server if user name is taken
+
+
+    // call to server to create user
+    // AxiosHelper.
   }
 
   function LoginDialogue() {
@@ -17,19 +37,19 @@ function Login(){
 
             <h1 className="li-title">Login</h1>
             <p className="li-form-title">Username</p>
-            <input className="li-form-input" type="text"/>
+            <input id="login-name" className="li-form-input" type="text"/>
             <p className="li-form-title">Passwort</p>
-            <input className="li-form-input" type="password"/>
-            <button className="li-form-button">Login</button>
+            <input id="login-pw" className="li-form-input" type="password"/>
+            <button className="li-form-button" onClick={() =>{login()}}>Login</button>
 
             <h1 className="li-title">Registrieren</h1>
             <p className="li-form-title">Username</p>
-            <input className="li-form-input" type="text"/>
+            <input id="register-name" className="li-form-input" type="text"/>
             <p className="li-form-title">Passwort</p>
-            <input className="li-form-input" type="password"/>
+            <input id="register-pw" className="li-form-input" type="password"/>
             <p className="li-form-title">Passwort wiederholen</p>
             <input className="li-form-input" type="password"/>
-            <button className="li-form-button">Regisrieren</button>
+            <button className="li-form-button" onClick={() =>{register()}}>Regisrieren</button>
           </div>
         </div>
       )

f95f51c4d91645080ccc237d8698ffa81c79b67f
Simon Weck <Simon.weck@raumtaenzer.com>
2021-10-20 00:45:50 +0200

    added hdr environment and new models to scene

--- src/components/BabylonView/BabylonView.js
+++ src/components/BabylonView/BabylonView.js
@@ -25,22 +25,60 @@ function BabylonView(){
 		camera.attachControl(myRef.current);
 
     // init Light
-    new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 1, 0), scene);
+    // new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 1, 0), scene);
 
     let assetsManager = new BABYLON.AssetsManager( scene );
     assetsManager.onTaskErrorObservable.add(function(task) {
       console.log('Task failed', task.errorObject.message, task.errorObject.exception);
     });
 
-    let phone;
-    var phoneTask = assetsManager.addMeshTask("", "", "","./models/logomodul.glb");
-    phoneTask.onSuccess = (task) => {     
-     phone = task.loadedMeshes[0];
-     phone.scaling = new BABYLON.Vector3(1000,1000,1000);
-     phone.name = "Phone";
+    // Loading Meshes
+    var plateTask = assetsManager.addMeshTask("", "", "","./models/plate.glb");
+    plateTask.onSuccess = (task) => {     
+     let plate = task.loadedMeshes[0];
+     plate.scaling = new BABYLON.Vector3(1000,1000,1000);
+     plate.name = "Plate";
     }
+
+    var moduleLTask = assetsManager.addMeshTask("", "", "","./models/module-large.glb");
+    moduleLTask.onSuccess = (task) => {     
+     let moduleL = task.loadedMeshes[0];
+     moduleL.scaling = new BABYLON.Vector3(1000,1000,1000);
+     moduleL.position = new BABYLON.Vector3(1,2,-55);
+     moduleL.name = "moduleL";
+    }
+
+    var moduleSTask = assetsManager.addMeshTask("", "", "","./models/module-small.glb");
+    moduleSTask.onSuccess = (task) => {     
+     let moduleS = task.loadedMeshes[0];
+     moduleS.scaling = new BABYLON.Vector3(1000,1000,1000);
+     moduleS.position = new BABYLON.Vector3(0,2,0);
+     moduleS.name = "moduleS";
+    }
+
+    var moduleXLTask = assetsManager.addMeshTask("", "", "","./models/module-xlarge.glb");
+    moduleXLTask.onSuccess = (task) => {     
+     let moduleXL = task.loadedMeshes[0];
+     moduleXL.scaling = new BABYLON.Vector3(1000,1000,1000);
+     moduleXL.position = new BABYLON.Vector3(0,2,0);
+     moduleXL.name = "moduleXL";
+    }
+
     assetsManager.load();
-    
+
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 2000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
+    skybox.infiniteDistance = true;
+    skyboxMaterial.disableLighting = true;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("https://assets.babylonjs.com/environments/studio.env", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
+    var hdrTexture = new BABYLON.CubeTexture.CreateFromPrefilteredData("https://assets.babylonjs.com/environments/studio.env", scene);
+    scene.environmentTexture = hdrTexture;
+
     engine.runRenderLoop(() => {
       scene.render();
     });

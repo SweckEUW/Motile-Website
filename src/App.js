4052f27af3a638c929af08f70a91571c26c925a7
Benjamin Jäger <benjamin-noor.jaeger@stud.hshl.de>
2021-10-30 15:12:24 +0200

    Navbar responsive fixes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import Panel from './components/Panel/Panel';
 import Login from './components/Login/Login';
 import BlenderRendering from './components/BlenderRendering/BlenderRendering';
 import Navbar from './components/Navbar/Navbar';
-import Head from './components/Home/Head';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/>
       <BlenderRendering/>
       <Panel/>
-      <BabylonView/>
-    {/* <Navbar/>
-    <Head/> */}
+      <BabylonView/> */}
+    <Navbar/>
+
     </div>
   );
 }

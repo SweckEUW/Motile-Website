aeb16dfd1c0fb662f821a6fed52d03d25a9c81d1
Paul <paul-julius.amelingmeyer@stud.hshl.de>
2021-11-09 14:21:39 +0100

    overview platzhalter

--- src/components/Configurator/Panel/Overview/Overview.js
+++ src/components/Configurator/Panel/Overview/Overview.js
@@ -7,6 +7,14 @@ function Overview(){
     <div className="Overview">
 
       <h2>Übersicht</h2>
+      <h1>Rüdiger<span className="material-icons">edit</span></h1>
+      <div class="box">
+        <h3><span>Rückseite</span></h3>        
+        <h4><span className="material-icons">memory</span>Soc<span className="price">50€</span></h4>
+        <h3><span>Vorderseite</span></h3>
+        <p>Gesamt 350€</p>
+      </div>
+
      
     </div>
   );

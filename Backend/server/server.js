cad108b904693d088fe0e36f06042aae65c816f3
SweckEUW <simon.weck98@yahoo.de>
2023-04-27 03:07:40 +0200

    test

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,16 +1,16 @@
 import express from "express"
 import cors from "cors"
 import mongodb from "mongodb"
-import multer from "multer"
+// import multer from "multer"
 import motilePartsCollection from "./motilePartsCollection.js"
 import UsersCollection from "./usersCollection.js"
-import BlenderJobs from "./blenderJobs.js"
+// import BlenderJobs from "./blenderJobs.js"
 import path from 'path';
 import { fileURLToPath } from 'url';
 import UserConfigsCollection from "./userConfigsCollection.js"
 import userDataCollection from "./userDataCollection.js"
-import Middleware from "./middleware.js"
-import ImageUploadHandler from "./ImageUploadHandler.js"
+// import Middleware from "./middleware.js"
+// import ImageUploadHandler from "./ImageUploadHandler.js"
 
 const app = express();
 
@@ -42,24 +42,24 @@ app.get('/MotileParts', motilePartsCollection.getMotileParts);
 
 // Login/Register
 app.post('/Login', UsersCollection.login);
-app.post('/LoginJWT', Middleware.verifyJWT, UsersCollection.loginJWT);
-app.post('/StayAlive', Middleware.verifyJWT, UsersCollection.stayAlive);
+// app.post('/LoginJWT', Middleware.verifyJWT, UsersCollection.loginJWT);
+// app.post('/StayAlive', Middleware.verifyJWT, UsersCollection.stayAlive);
 app.post('/Register', UsersCollection.addUser);
 app.get('/VerifyEmail', UsersCollection.verifyUser);
 
 // UserData
-app.post('/User/Data', Middleware.verifyJWT, UsersCollection.getUserDataFromUser);
-app.post('/User/Data/AddAddress', Middleware.verifyJWT, userDataCollection.addAddress);
-app.post('/User/Data/RemoveAddress', Middleware.verifyJWT, userDataCollection.removeAddress);
-app.post('/User/Data/UploadProfilePic',  multer({ storage: ImageUploadHandler.getStorage() }).single('file'), Middleware.verifyJWT, userDataCollection.updateProfilePic);
-app.post('/User/Data/Modify', Middleware.verifyJWT, userDataCollection.modifyUserData);
+// app.post('/User/Data', Middleware.verifyJWT, UsersCollection.getUserDataFromUser);
+// app.post('/User/Data/AddAddress', Middleware.verifyJWT, userDataCollection.addAddress);
+// app.post('/User/Data/RemoveAddress', Middleware.verifyJWT, userDataCollection.removeAddress);
+// app.post('/User/Data/UploadProfilePic',  multer({ storage: ImageUploadHandler.getStorage() }).single('file'), Middleware.verifyJWT, userDataCollection.updateProfilePic);
+// app.post('/User/Data/Modify', Middleware.verifyJWT, userDataCollection.modifyUserData);
 
 // UserConfigurations
-app.post('/User/Configs', Middleware.verifyJWT, UsersCollection.getConfigFromUser);
-app.post('/User/Configs/Remove', Middleware.verifyJWT, UserConfigsCollection.removeUserConfiguration);
-app.post('/User/Configs/Add', Middleware.verifyJWT, UserConfigsCollection.addUserConfiguration);
-app.post('/User/Configs/GenerateThumbnail', Middleware.verifyJWT, BlenderJobs.renderThumbnail);
-app.post('/User/Configs/Buy', Middleware.verifyJWT, UserConfigsCollection.setUserConfigToBought)
+// app.post('/User/Configs', Middleware.verifyJWT, UsersCollection.getConfigFromUser);
+// app.post('/User/Configs/Remove', Middleware.verifyJWT, UserConfigsCollection.removeUserConfiguration);
+// app.post('/User/Configs/Add', Middleware.verifyJWT, UserConfigsCollection.addUserConfiguration);
+// app.post('/User/Configs/GenerateThumbnail', Middleware.verifyJWT, BlenderJobs.renderThumbnail);
+// app.post('/User/Configs/Buy', Middleware.verifyJWT, UserConfigsCollection.setUserConfigToBought)
 
 // static assets - public folder
 let filename = fileURLToPath(import.meta.url);

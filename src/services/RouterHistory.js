5df19c04640482443618a367832f688ebede626c
SweckEUW <simon.weck98@yahoo.de>
2023-04-28 15:43:23 +0200

    test

--- src/services/RouterHistory.js
+++ src/services/RouterHistory.js
@@ -1,3 +1,3 @@
 import { createBrowserHistory } from 'history';
 
-export default createBrowserHistory({basename: 'Motile-Website', forceRefresh: false});
+export default createBrowserHistory({basename: 'Motile-Website'});
